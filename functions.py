033309a355a1132425e8567430b570d858133f59
kaue piovan <kaupiovan@gmail.com>
2023-09-13 16:07:35 -0300

    funcao add, view e remove

--- functions.py
+++ functions.py
@@ -1,21 +1,59 @@
+import os
+
+# view task
+def view(todo_list):
+    os.system('cls')
+
+    tasks_done = []
+    tasks_peding = []
+
+    # separas as tasks feitas das não feitas
+    for task, status in todo_list:
+        print(task)
+
+        if status == True:
+            tasks_done.append(task)
+        else:
+            tasks_peding.append(task)
+
+    # print('Tarefas a fazer: ')
+    # for task in tasks_peding:
+    #     print(task)
+
+    # print('Tarefas feitas: ')
+    # for task in tasks_done:
+    #     print(task)
+
+    return None
+
+
 # add task
+def add(task_name, todo_list):
+
+    todo_list.append((f' - [ ] {task_name}', False))
+
+    return None
+
 
 # remove task
+def remove(task_name, todo_list):
+    if task_name.isdigit():
+        index = int(task_name) - 1
+        todo_list.pop(index)
+
+    elif isinstance(task_name, str):
+        for i in todo_list:
+            if task_name in i:
+                todo_list.remove(i)
+            else:
+                continue
+    
+    return None
+
 
 # update task
-
+def update():
+    ...
 # help
 def help():
-    print('''
-
-        COMANDOS:
-        add -> add <Nome da task>
-            adiciona uma nova task para a lista de todo
-        
-        del -> del <nome da task>
-            remove a task da lista de todo
-        
-        upd -> upd <nome da task>
-            atualiza a task da lista de todo
-
-    ''')
+    ...
--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 import functions
 
-user_input = user_input_split = command = task_name = ''
+todo_list = []
+user_input = user_input_split = ''
+command = task_name = ''
 
-# Armazena a entrada do usuario
-user_input = str(input()).strip()
-user_input_split = user_input.split(' ')
+while True:
 
-# Tira somente comando e guarda na variavel
-command = user_input_split.pop(0)
+    # Mostra na tela todas as task adicionadas
+    functions.view(todo_list)
+    
+    # Armazena a entrada do usuario
+    user_input = str(input()).strip()
+    user_input_split = user_input.split(' ')
 
-# guarda na variavel o nome da task
-task_name = ' '.join(user_input_split[:])
+    # Tira somente comando e guarda na variavel
+    command = user_input_split.pop(0)
+
+    # guarda na variavel o nome da task
+    task_name = ' '.join(user_input_split[:])
+
+    # seleciona o comando desejado pelo usuario
+    if command == 'add':
+        functions.add(task_name, todo_list)
+
+    elif command == 'remove':
+        functions.remove(task_name, todo_list)
+    elif command == 'upd':
+        ...
+    else:
+        ...
