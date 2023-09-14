7a59fa2d1224d6913f28aa0c05db086e349099ad
kaue piovan <kaupiovan@gmail.com>
2023-09-14 15:04:03 -0300

    concertando as funções

--- functions.py
+++ functions.py
@@ -3,26 +3,11 @@ import os
 # view task
 def view(todo_list):
     os.system('cls')
+    c = 0
 
-    tasks_done = []
-    tasks_peding = []
-
-    # separas as tasks feitas das não feitas
-    for task, status in todo_list:
-        print(task)
-
-        if status == True:
-            tasks_done.append(task)
-        else:
-            tasks_peding.append(task)
-
-    # print('Tarefas a fazer: ')
-    # for task in tasks_peding:
-    #     print(task)
-
-    # print('Tarefas feitas: ')
-    # for task in tasks_done:
-    #     print(task)
+    for task in todo_list:
+        c += 1
+        print(c, task[0])
 
     return None
 
@@ -30,24 +15,33 @@ def view(todo_list):
 # add task
 def add(task_name, todo_list):
 
-    todo_list.append((f' - [ ] {task_name}', False))
+    todo_list.append([f'- [ ] {task_name}', False])
 
     return None
 
 
 # remove task
 def remove(task_name, todo_list):
+    
+    flag = True
     if task_name.isdigit():
         index = int(task_name) - 1
         todo_list.pop(index)
+        flag = False
 
     elif isinstance(task_name, str):
+        task_name = f'- [ ] {task_name}'
         for i in todo_list:
             if task_name in i:
                 todo_list.remove(i)
+                flag = False
+                break
             else:
                 continue
-    
+    if flag:
+        print('Erro digite o indice ou o nome de uma tarefa existente.')
+        input('Aperte enter para continuar.')      
+
     return None
 
 
